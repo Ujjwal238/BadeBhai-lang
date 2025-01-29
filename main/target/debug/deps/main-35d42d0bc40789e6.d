2071026c28e7efc5888957dca1600592c4bbf8fc
Ujjwal238 <ujjwalmishra238@gmail.com>
2025-01-30 01:44:53 +0530

    added main

---
+++ main/target/debug/deps/main-35d42d0bc40789e6.d
@@ -0,0 +1,5 @@
+/home/ujjwal/Personal/Acadmics/Projects/BadeBhai-lang/main/target/debug/deps/libmain-35d42d0bc40789e6.rmeta: src/main.rs
+
+/home/ujjwal/Personal/Acadmics/Projects/BadeBhai-lang/main/target/debug/deps/main-35d42d0bc40789e6.d: src/main.rs
+
+src/main.rs:
---
+++ main/target/debug/deps/main-48c40b6064c74ba9.d
@@ -0,0 +1,5 @@
+/home/ujjwal/Personal/Acadmics/Projects/BadeBhai-lang/main/target/debug/deps/libmain-48c40b6064c74ba9.rmeta: src/main.rs
+
+/home/ujjwal/Personal/Acadmics/Projects/BadeBhai-lang/main/target/debug/deps/main-48c40b6064c74ba9.d: src/main.rs
+
+src/main.rs:
